f7458ab8794d0e5f8effaebe765fc36134bec35c
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-05-16 22:27:05 -0400

    funciones que funcionan!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
-function saludo() {
-    document.getElementById("demo").addEventListener("click", function() {
-        alert("Hola Mundo!");
-    });
+//Al cargar la página lanza un alert
+window.onload = function() {
+    alert("Esta página está cargada!");
 }
+
+//Al hacer click en el botón envía un alert
+var botoncito = document.getElementById("demo");
+botoncito.addEventListener("click", function() {
+    alert("Hola Mundo!");
+});
+
+//Al pasar sobre el campo de texto envía un alert
+var campoText = document.getElementById("input");
+campoText.addEventListener("mouseover", function() {
+    alert("Eso da cosquillas!!!!");
+});
+
+//Al dejar de pasar sobre el campo de texto envía un alert
+var campoText = document.getElementById("input");
+campoText.addEventListener("mouseout", function() {
+    alert("Gracias! soy muy cosquillosa!!!");
+});
